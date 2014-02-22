a20b363da561b658185aa9dc7d0e645eea3e5433
technomagus <contato@technomagus.com.br>
2014-02-22 12:39:56 -0300

    foi adicionado bordas na visualização dos modelos

--- themes/curv/views/modalidades/modalidadesIndex.php
+++ themes/curv/views/modalidades/modalidadesIndex.php
@@ -3,7 +3,7 @@
 <section class="cols">
     <?php foreach($dataProvider as $data)
 {  ?>
-        <div class="col">
+        <div class="col" style="border: black 1px solid;">
             <?php echo CHtml::link(CHtml::image($data->MOD_IMAGE, $data->MOD_TITULO, array('title'=>$data->MOD_TITULO,'width'=>'80px', 'class'=>'modInicial', 'style'=>'border-radius: 1em;')), Yii::app()->createUrl('/modalidades/ver', array('id'=>$data->MODALIDADE_ID))); ?>
             <div class="col-cnt">
                 <h2><?php echo $data->MOD_TITULO;?></h2>
